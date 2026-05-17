a4c0e5f46ecb838f61116b7b9f154acf29a86930
A. Maslarov <nsite2014@gmail.com>
2026-05-18 00:11:04 +0300

    feat: add root layout component to application structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,6 +97,7 @@ export default function RootLayout({
           </div>
 
         </footer>
+        <script data-host="https://count.bg" data-dnt="false" src="https://count.bg/js/script.js" id="ZwSg9rf6GA" async defer></script>
       </body>
     </html>
   );
